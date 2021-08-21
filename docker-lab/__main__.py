c9fa3559a96ef5fb199139c170127e6b4df375ef
Kunal <knkillekar@gmail.com>
2021-08-21 16:26:57 +0530

    updated labs

--- docker-lab/__main__.py
+++
@@ -1,9 +0,0 @@
-import pulumi_docker as docker
-
-image = docker.RemoteImage("ubuntu",
-                           name='ubuntu:precise'
-                           )
-
-container = docker.Container("ubuntu",
-                             image=image.latest
-                             )
