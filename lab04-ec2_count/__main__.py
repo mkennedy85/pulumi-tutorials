ad83d5aeba7aedb5aacbe7457897c6870eb2674e
Chandan Kumar <chandank@becloudready.com>
2021-08-23 18:19:00 -0400

    Changed ami to ca-central-1

--- lab04-ec2_count/__main__.py
+++ lab04-ec2_count/__main__.py
@@ -5,12 +5,14 @@ import pulumi_aws as aws
 web = []
 for range in [{"value": i} for i in range(0, 2)]:
     web.append(aws.ec2.Instance(f"web-{range['value']}",
-                                ami="ami-04db49c0fb2215364",
+                                ami="ami-02f84cf47c23f1769",
                                 instance_type="t2.micro",
                                 tags={
                                     "Name": f"web-{range['value']}",
                                 }))
 
+config = pulumi.Config()
 pulumi.export('web-0-publicIP', web[0].public_ip)
 pulumi.export('web-1-publicIP', web[1].public_ip)
+pulumi.export("DB password",config.get("dbpass"))
 
