937453aecd7d351898f2396c8d167dc9947e7b7f
Chandan Kumar <chandank@becloudready.com>
2021-08-23 19:28:42 -0400

    added bucket name

--- lab19-s3-variables/__main__.py
+++ lab19-s3-variables/__main__.py
@@ -5,7 +5,14 @@ from pulumi_aws import s3
 
 # Create an AWS resource (S3 Bucket)
 stack = pulumi.get_stack()
-bucket = s3.Bucket("my-bucket-"+pulumi.get_stack())
+config = pulumi.Config("aws")
+aws_config = config.get("region")
+project_config = pulumi.Config()
+bucket_name = project_config.get("bucket_name")
+print("Region",aws_config)
+
+
+bucket = s3.Bucket(bucket_name +pulumi.get_stack())
 
 # Export the name of the bucket
 pulumi.export('bucket_name', bucket.id)
