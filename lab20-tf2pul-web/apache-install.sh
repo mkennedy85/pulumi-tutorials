41703ff0bb441d0730cc5576c36319117bff27ac
Kunal <knkillekar@gmail.com>
2021-08-24 18:26:48 +0530

    updated

--- lab20-tf2pul-web/apache-install.sh
+++ lab20-tf2pul-web/apache-install.sh
@@ -3,4 +3,4 @@ sudo yum update -y
 sudo yum install -y httpd
 sudo systemctl enable httpd
 sudo service httpd start  
-echo "<h1>Welcome to Pulumi Training ! AWS Infra created using Pulumi in us-east-1 Region</h1>" | sudo tee /var/www/html/index.html
+echo "<h1>Welcome to Pulumi Training ! AWS Infra created using Pulumi in us-east-1 Regions</h1>" | sudo tee /var/www/html/index.html
