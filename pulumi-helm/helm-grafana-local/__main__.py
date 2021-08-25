c197108cb38ee162d478ea718330adc979946ffc
Chandan <chandank@live.ca>
2021-08-25 11:33:03 -0400

    Update __main__.py

--- pulumi-helm/helm-grafana-local/__main__.py
+++ pulumi-helm/helm-grafana-local/__main__.py
@@ -3,6 +3,6 @@ from pulumi_kubernetes.helm.v3 import Chart, LocalChartOpts
 grafana = Chart(
     "grafana",
     LocalChartOpts(
-        path="C:/Users/kunal/pulumi-tutorials/helm-grafana/grafana",
+        path="grafana",
     ),
 )
